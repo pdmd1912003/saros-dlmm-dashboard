8d34336d50562f3f726d03cb848a8e1224ffb89a
pdmd1912003 <pdmd1912003@gmail.com>
2025-10-17 14:37:06 +0700

    move folder

--- pages/api/portfolio.ts
+++
@@ -1,7 +0,0 @@
-import handler from "../../backend/api/portfolio";
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default async function apiRoute(req: NextApiRequest, res: NextApiResponse) {
-  // Delegate to the existing backend handler
-  return handler(req, res);
-}
